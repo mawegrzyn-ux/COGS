e3c40e6ca4133182147563460b7afe63099be6de
mawegrzyn-ux <ma.wegrzyn@gmail.com>
2026-03-05 03:36:55 +0000

    Update ColumnHeader.tsx

--- app/src/components/ColumnHeader.tsx
+++ app/src/components/ColumnHeader.tsx
@@ -25,7 +25,7 @@ export function ColumnHeader<T>({
   sortDir,
   onSort,
   filterOptions,
-  filterValues = [],
+  filterValues = [] as string[],
   onFilter,
   align = 'left',
 }: ColumnHeaderProps<T>) {
